f388e061e058046d3a340c63dc639845f17b4da3
gubersky <https://github.com/gubersky/Burger.git>
2023-03-08 15:23:40 +0200

    update

--- src/main/java/home_works/home_work3/MyFirstSolution.java
+++ src/main/java/home_works/home_work3/MyFirstSolution.java
@@ -7,11 +7,9 @@ public class MyFirstSolution {
         for (int i = 0; i < s.length(); i++) {
             String check = String.valueOf(t.charAt(i));
             if (!s.contains(check)) return false;
-            for (int k = 0; k < t.length(); k++) {
-                int firstCount = s.length() - s.replace(check, "").length();
-                int secondCount = t.length() - t.replace(check, "").length();
-                if (firstCount != secondCount) return false;
-            }
+            int firstCount = s.length() - s.replace(check, "").length();
+            int secondCount = t.length() - t.replace(check, "").length();
+            if (firstCount != secondCount) return false;
         }
         return true;
     }
