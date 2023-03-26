91344327783eda6bda9ee15a00bb7dcaec577cc6
gubersky <https://github.com/gubersky/Burger.git>
2023-03-26 12:51:25 +0300

    refactor

--- src/main/java/home_works/home_work6/task1/Solution.java
+++ src/main/java/home_works/home_work6/task1/Solution.java
@@ -3,7 +3,7 @@ package home_works.home_work6.task1;
 public class Solution {
     public boolean isPalindrome(String word) {
         String removeSymbols = word.replaceAll("\\W","").replace("_","").toLowerCase();
-        StringBuilder stringBuilder = new StringBuilder(removeSymbols);
-        return removeSymbols.equals(stringBuilder.reverse().toString());
+        StringBuilder reverseWord = new StringBuilder(removeSymbols);
+        return removeSymbols.equals(reverseWord.reverse().toString());
     }
 }
